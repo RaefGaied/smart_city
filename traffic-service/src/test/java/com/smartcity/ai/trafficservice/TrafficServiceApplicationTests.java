dd0567cd62fd0b564377d8c0d3b1e42dfc8d1a78
Raef Gaied <raef.gaied@polytechnicien.tn>
2025-12-30 20:29:32 +0100

    Add initial project structure and configuration files for traffic-service

---
+++ traffic-service/src/test/java/com/smartcity/ai/trafficservice/TrafficServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcity.ai.trafficservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
